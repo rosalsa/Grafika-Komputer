0833aebca3ea575827cf053344dbc1a931f8e8c7
Rosalia Salsabila <152388107+rosalsa@users.noreply.github.com>
2026-04-13 17:18:58 +0700

    animasi 3d part 1 (sumbu x, y, z)

---
+++ P5/sumbu3d.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Membuat titik acak dalam ruang 3D
+num_points = 100
+x = np.random.rand(num_points) * 100
+y = np.random.rand(num_points) * 100
+z = np.random.rand(num_points) * 100
+
+# Menampilkan titik dalam ruang 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x, y, z, c='r', marker='o')
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+ax.set_title("Visualisasi sumbu X, Y dan Z")
+
+plt.show()
